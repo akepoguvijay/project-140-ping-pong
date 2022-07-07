e2aa7741e0f1cbb8be652ec761d56d870a282a66
akepoguvijay <85406979+akepoguvijay@users.noreply.github.com>
2022-07-07 21:13:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
+
+function preload() 
+{
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+  
+function setup()
+{
+  canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() 
+{
+  console.log('PoseNet Is Initialized');
+}
+
+function draw()
+{
+  image(video,0,0,700,600);
+}
+
+function gotPoses()
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
+}
+  
